92a6c5c29c3544217cefcde3c383dc78a3dab03c
Aditya Raj <araj67995@gmail.com>
2025-03-18 18:49:48 +0530

    only basic

---
+++ app.js
@@ -0,0 +1,14 @@
+function updateProgress(remainingPercent) {
+    const circle = document.querySelector('.progress-ring-fill');
+    const radius = 140;
+    const circumference = 2 * Math.PI * radius;
+    
+    const offset = (remainingPercent / 100) * circumference;
+    circle.style.strokeDashoffset = circumference - offset;
+  
+    document.querySelector('.progress-text').innerText = remainingPercent + "%";
+  }
+  
+  // Example: Set progress to 50%
+  updateProgress(10);
+  
